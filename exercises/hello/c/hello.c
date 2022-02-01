d8c6d98d75cc713a14a3337e4452ff73eee72df9
Mads Hansen Baattrup <mads@baattrup.com>
2022-02-01 19:38:56 +0100

    Updated solution

--- exercises/hello/c/hello.c
+++ exercises/hello/c/hello.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 
 int main(){
 	// Declare pointer to hold username
 	char *buf;
 	// Allocate memory
-	buf = (char *) malloc(10*sizeof(char));
-	// Get user name
-	buf = getlogin();
+	buf = (char *) malloc(10*sizeof(buf));
+	// Get user name by copying the return value of
+	// getlogin() into the memoery pointer created by malloc
+	strcpy(buf, getlogin());
 	// Print
 	printf("Hello, %s!\n", buf);
+	// Deallocate memory
+	free(buf);
 	return 0;
 }
