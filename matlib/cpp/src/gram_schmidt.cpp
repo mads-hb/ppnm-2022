a64eda66372a8f2a77059afec5865870f7410aad
Mads Hansen Baattrup <mads@baattrup.com>
2022-03-19 20:13:01 +0100

    Fix error in GS decompose related to wrong dimensions being used.

--- matlib/cpp/src/gram_schmidt.cpp
+++ matlib/cpp/src/gram_schmidt.cpp
@@ -1,5 +1,5 @@
+#include <memory>
 #include "cmath"
-#include "memory"
 #include "gram_schmidt.hpp"
 #include "cassert"
 
@@ -7,13 +7,13 @@
 void gram_schmidt::decompose(Matrix *A, Matrix *R) {
     assert(A->get_height()>=A->get_length());
     // Create matrix Q
-    auto Q = std::unique_ptr<Matrix>(new Matrix(A->get_length(), A->get_height()));
+    auto Q = std::make_unique<Matrix>(A->get_height(), A->get_length());
 
     // Iterate over second dimension of matrix - columns.
-    for (int i = 0; i < A->get_height(); ++i){
+    for (int i = 0; i < A->get_length(); ++i){
         // Compute the norm of the column a_i of matrix A.
         double ai_norm = 0;
-        for (int j = 0; j < A->get_length(); ++j){
+        for (int j = 0; j < A->get_height(); ++j){
             ai_norm += pow(A->get(j, i), 2);
         }
         ai_norm = sqrt(ai_norm);
@@ -22,21 +22,21 @@ void gram_schmidt::decompose(Matrix *A, Matrix *R) {
         R->set(i, i, ai_norm);
 
         // Set the column Q_i to a_i divided by the ai_norm computed before.
-        for (int j = 0; j < Q->get_length(); ++j){
+        for (int j = 0; j < Q->get_height(); ++j){
             double value = A->get(j, i) / ai_norm;
             Q->set(j, i, value);
         }
 
         // Enter second loop
-        for (int j = i; j < A->get_height(); ++j){
+        for (int j = i; j < A->get_length(); ++j){
             // Compute the product q_i and a_j and set R_ij equal to it.
             double q_dot_a = 0;
-            for (int k = 0; k < Q->get_length(); ++k){
+            for (int k = 0; k < Q->get_height(); ++k){
                 q_dot_a += Q->get(k, i) * A->get(k, j);
             }
             R->set(i, j, q_dot_a);
 
-            for (int k = 0; k < A->get_length(); ++k){
+            for (int k = 0; k < A->get_height(); ++k){
                 double val = A->get(k, j) - q_dot_a * Q->get(k, i);
                 A->set(k, j, val);
             }
@@ -45,8 +45,8 @@ void gram_schmidt::decompose(Matrix *A, Matrix *R) {
     }
 
     // Copy Q into A
-    for (int i = 0; i < A->get_length(); ++i){
-        for (int j = 0; j < A->get_height(); ++j){
+    for (int i = 0; i < A->get_height(); ++i){
+        for (int j = 0; j < A->get_length(); ++j){
             A->set(i, j, Q->get(i, j));
         }
     }
